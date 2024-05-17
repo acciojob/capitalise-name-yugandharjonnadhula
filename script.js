57ff457790a1714a866b5171de72fdceee39f92a
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-17 17:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const inputfield=document.getElementById("fname");
+
+inputfield.addEventListener("blur",function(){
+	const value=inputfield.value;
+	const upperCaseValue=value.toUpperCase();
+	inputfield.value=upperCaseValue;
+	
+})
